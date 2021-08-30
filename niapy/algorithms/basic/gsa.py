4d5415f78dd60dd9a9c07ac4c262e54794a94738
chinmay3 <74144636+chinmay3@users.noreply.github.com>
2021-08-30 14:19:29 +0530

    vim command removed

--- niapy/algorithms/basic/gsa.py
+++ niapy/algorithms/basic/gsa.py
@@ -172,4 +172,4 @@ class GravitationalSearchAlgorithm(Algorithm):
         best_x, best_fitness = self.get_best(population, population_fitness, best_x, best_fitness)
         return population, population_fitness, best_x, best_fitness, {'velocities': velocities}
 
-# vim: tabstop=3 noexpandtab shiftwidth=3 softtabstop=3
+
